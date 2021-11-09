5af57c80ccb6c14f58c025fb7a77a5f2c3ff9896
Jamie Bowers <jamiebowers3@gmail.com>
2021-11-09 12:30:46 +0000

    User story 6 - Implemented description/price per night to database

--- db/migrations/01_makers_bnb.sql
+++ db/migrations/01_makers_bnb.sql
@@ -1,1 +1,1 @@
-CREATE TABLE spaces(id SERIAL PRIMARY KEY, name VARCHAR(60)) ;
+CREATE TABLE spaces(id SERIAL PRIMARY KEY, name VARCHAR(60), description VARCHAR(60), price_per_night DOUBLE PRECISION) ;
--- spec/database_helper.rb
+++ spec/database_helper.rb
@@ -1,4 +1,10 @@
 require 'PG'
+
+def reset_db
+  connection = PG.connect(dbname: 'makers_bnb_test')
+  connection.exec('TRUNCATE spaces;')
+end 
+
 def connect_to_db
   connection = PG.connect(dbname: 'makers_bnb_test')
 end 
