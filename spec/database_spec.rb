287acb8b096c3d120afaec32a18781182f0f0375
Jamie Bowers <jamiebowers3@gmail.com>
2021-11-10 11:01:27 +0000

    database_spec - unit test for .query implemented

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -12,5 +12,17 @@ describe Database do
 
   end 
 
+  describe '.query' do 
+
+    it 'checks we can retrieve items from the database' do 
+
+      Database.connect('makers_bnb')
+      Database.query("INSERT INTO spaces(space_name, space_description, price_per_night) VALUES('Bat Cave', 'Its a bat cave', 2.55);")
+      expect(Database.query('SELECT space_name FROM spaces').first['space_name']).to eq "Bat Cave"
+
+    end 
+
+  end 
+
 
 end 
