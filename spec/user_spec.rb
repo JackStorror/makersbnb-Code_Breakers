4ac0651c44b8dea138924ca612db82106c7e801a
Jack Arthur Storror <jackarthurstorror@Jacks-MacBook-Pro.local>
2021-11-11 11:31:31 +0000

    should be able to merge now

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,9 +1,10 @@
 require_relative '../model/user_handler'
 describe UserHandler do
   describe '.create_user' do
-    it 'creates a new user' do
-      UserHandler.create_user('Batman')
-      expect(UserHandler.get_user_name).to eq 'Batman'
+    it 'creates a new user with encrypted password' do
+      expect(BCrypt::Password).to receive(:create).with('Gotham')
+      User.create_user(user_name:'Batman', password:'Gotham')
+      expect(User.get_user_name).to eq 'Batman'
     end
   end
 end
