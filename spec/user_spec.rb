8c20493b78455b7f7b6809a4656f1625977697b3
Jack Arthur Storror <jackarthurstorror@Jacks-MacBook-Pro.local>
2021-11-11 11:37:43 +0000

    changes

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,8 +3,8 @@ describe UserHandler do
   describe '.create_user' do
     it 'creates a new user with encrypted password' do
       expect(BCrypt::Password).to receive(:create).with('Gotham')
-      User.create_user(user_name:'Batman', password:'Gotham')
-      expect(User.get_user_name).to eq 'Batman'
+      UserHandler.create_user(user_name:'Batman', password:'Gotham')
+      expect(UserHandler.get_user_name).to eq 'Batman'
     end
   end
 end
