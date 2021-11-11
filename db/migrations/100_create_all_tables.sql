761a04e7a29bd70a0550a7aed963e617179e93fe
Jenaro Garcia <jjegarcia@hotmail.com>
2021-11-11 12:26:13 +0000

    truncation fixed

---
+++ db/migrations/100_create_all_tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE spaces
+(
+    space_id          SERIAL PRIMARY KEY,
+    space_name        VARCHAR(20),
+    space_description VARCHAR(120),
+    price_per_night   DOUBLE PRECISION
+);
+
+CREATE TABLE users
+(
+    user_id   SERIAL PRIMARY KEY,
+    user_name VARCHAR(60) UNIQUE,
+    space_id   BIGINT REFERENCES spaces (space_id)
+);
+
+
+CREATE TABLE bookings
+(
+    booking_id    serial PRIMARY KEY,
+    space_id      INTEGER REFERENCES spaces (space_id),
+    booking_start DATE NOT NULL,
+    booking_end   DATE NOT NULL
+);
