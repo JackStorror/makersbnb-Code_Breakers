553205abe6493ad15073d4f90ef0033d564475d7
Jack Arthur Storror <jackarthurstorror@Jacks-MacBook-Pro.local>
2021-11-12 10:52:28 +0000

    db file changes as requested

--- db/migrations/100_create_all_tables.sql
+++ db/migrations/100_create_all_tables.sql
@@ -11,8 +11,7 @@ CREATE TABLE spaces
     space_name        VARCHAR(20),
     space_description VARCHAR(120),
     price_per_night   DOUBLE PRECISION,
-    user_id           INTEGER REFERENCES users (user_id)
-
+    user_id           INTEGER NOT NULL REFERENCES users (user_id),
 );
 
 CREATE TABLE bookings
@@ -20,5 +19,16 @@ CREATE TABLE bookings
     booking_id    serial PRIMARY KEY,
     space_id      INTEGER REFERENCES spaces (space_id),
     booking_start DATE NOT NULL,
-    booking_end   DATE NOT NULL
-);
+    booking_end   DATE NOT NULL,
+    host_id       INTEGER NOT NULL REFERENCES users (user_id),
+    user_id       INTEGER NOT NULL REFERENCES users (user_id)
+  );
+
+  CREATE TABLE requests (
+    request_id serial PRIMARY KEY,
+    space_id INTEGER REFERENCES spaces (space_id),
+    request_start DATE NOT NULL,
+    request_end DATE NOT NULL,
+    host_id INTEGER NOT NULL REFERENCES users (user_id),
+    user_id INTEGER NOT NULL REFERENCES users (user_id)
+  );
