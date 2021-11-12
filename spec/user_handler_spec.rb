37d96c55b5c0e36ea86c6ac2db12cda80e012076
Jack Arthur Storror <jackarthurstorror@Jacks-MacBook-Pro.local>
2021-11-12 10:46:07 +0000

    user .rb deleted and user_spec deleted

--- spec/user_handler_spec.rb
+++ spec/user_handler_spec.rb
@@ -1,11 +1,11 @@
 require_relative '../model/user_handler'
 require_relative './database_helper'
 describe UserHandler do
-  it "should get user's id" do
-
+  describe '.create_user' do
+    it 'creates a new user with encrypted password' do
+      expect(BCrypt::Password).to receive(:create).with('Gotham')
+      UserHandler.create_user(user_name:'Batman', password:'Gotham')
+      expect(UserHandler.find_user_name).to eq 'Batman'
+    end
   end
-  it "should get user's name" do
-
-  end
-
 end
